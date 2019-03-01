82aefe96c493892e001a261b31d6ebc618441743
Binaya Rimal <rimal.binaya@gmail.com>
2019-03-01 14:40:30 -0800

    completed cp2

---
+++ src/config/route-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  init(app){
+    const staticRoutes = require("../routes/static");
+    const marco= require ("../routes/marco");
+    app.use(marco);
+    app.use(staticRoutes);
+  }
+
+}
