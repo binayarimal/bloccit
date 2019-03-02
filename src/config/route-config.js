48fcdb09016dc965706f9d4fc13cfb2f9bab106a
Binaya Rimal <rimal.binaya@gmail.com>
2019-03-01 17:40:20 -0800

    changes made

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,8 +2,10 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const marco= require ("../routes/marco");
+
     app.use(marco);
-    app.use(staticRoutes);
+      app.use(staticRoutes);
+
   }
 
 }
