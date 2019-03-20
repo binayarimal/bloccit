59a18d1dfd2c9429882c0c172f780bd517087108
Binaya Rimal <rimal.binaya@gmail.com>
2019-03-20 12:40:02 -0700

    added

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,5 +1,19 @@
-module.exports = {
-  index(req, res, next){
-    res.send("TODO: list all topics");
-  }
-}
+const topicQueries = require("../db/queries.topics.js");
+const sequelize = require("../../src/db/models/index").sequelize;
+ const Topic = require("../../src/db/models").Topic;
+ module.exports = {
+   index(req, res, next){
+
+
+//#2
+      topicQueries.getAllTopics((err, topics) => {
+
+//#3
+        if(err){
+          res.redirect(500, "static/index");
+        } else {
+          res.render("topics/index", {topics});
+        }
+      })
+   }
+ }
