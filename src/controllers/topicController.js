b086b94b3002624065a35e684c1160d05ff5e740
Binaya Rimal <rimal.binaya@gmail.com>
2019-03-22 12:59:12 -0700

    topic controller not working

--- src/controllers/topicController.js
+++ src/controllers/topicController.js
@@ -1,18 +1,58 @@
 const topicQueries = require("../db/queries.topics.js");
-
- module.exports = {
-   index(req, res, next){
-
-
-//#2
-      topicQueries.getAllTopics((err, topics) => {
-
-//#3
-        if(err){
-          res.redirect(500, "static/index");
-        } else {
-          res.render("topics/index", {topics});
+module.exports = {
+  index(req, res, next){
+    topicQueries.getAllTopics((err, topics) => {
+      if(err){
+        res.redirect(500, "static/index");
+      } else {
+        res.render("topics/index", {topics});
+      }
+    })
+  };
+  create(req, res, next){
+    let newTopic = {
+      title: req.body.title,
+      description: req.body.description
+    };
+    topicQueries.addTopic(newTopic, (err, topic) => {
+      if(err){
+        res.redirect(500, "/topics/new");
+      } else {
+        res.redirect(303, `/topics/${topic.id}`);
+      }
+    });
+  };
+  new(req, res, next){
+    res.render("topics/new");
+  };
+  show(req, res, next){
+    topicQueries.getTopic(req.params.id, (err, topic) => {
+      if(err || topic == null){
+        res.redirect(404, "/");}
+        else {
+          res.render("topics/show", {topic});
         }
-      })
-   }
- }
+      });
+    };
+    edit(req, res, next){
+      topicQueries.getTopic(req.params.id, (err, topic) => {
+        if(err || topic == null){
+          res.redirect(404, "/");
+        } else {
+          res.render("topics/edit", {topic});
+        }
+      });
+    };
+    update(req, res, next){
+      topicQueries.updateTopic(req.params.id, req.body, (err, topic) => {
+
+        //#2
+        if(err || topic == null){
+          res.redirect(404, `/topics/${req.params.id}/edit`);
+        } else {
+          res.redirect(`/topics/${topic.id}`);
+        }
+      });
+    }
+
+  }
--- src/routes/topics.js
+++ src/routes/topics.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const topicController = require("../controllers/topicController")
-
+router.get("/topics/new", topicController.new);
 router.get("/topics", topicController.index);
-
+router.get("/topics/:id", topicController.show);
+router.post("/topics/:id/destroy", topicController.destroy);
+router.get("/topics/:id/edit", topicController.edit);
+router.post("/topics/:id/update", topicController.update);
 module.exports = router;
