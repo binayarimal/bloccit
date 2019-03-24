ac977064bf957d2c1eadc5b4eac80135ac546edf
Binaya Rimal <rimal.binaya@gmail.com>
2019-03-24 15:53:00 -0700

    cp completed

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,21 +1,30 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topic = sequelize.define('Topic', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+         type: DataTypes.STRING,
+         allowNull: false
+       },
+    description: {
+            type: DataTypes.STRING,
+            allowNull: false
+          },
   }, {});
   Topic.associate = function(models) {
     Topic.hasMany(models.Banner, {
-    foreignKey: "topicId",
-    as: "banners",
-  });
-  };
-  Topic.associate = function(models) {
+      foreignKey: "topicId",
+      as: "banners",
+    });
     Topic.hasMany(models.Rule, {
-    foreignKey: "topicId",
-    as: "rules",
-  });
+      foreignKey: "topicId",
+      as: "rules",
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts",
+    });
   };
+
 
   return Topic;
 };
