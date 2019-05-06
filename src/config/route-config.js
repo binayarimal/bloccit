7cf087f1ab61a4312dac23d7f27e3f726bde4193
Binaya Rimal <rimal.binaya@gmail.com>
2019-05-05 17:03:47 -0700

    about deleted

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,7 +2,6 @@ module.exports = {
   init(app){
     const staticRoutes = require("../routes/static");
     const marco= require ("../routes/marco");
-    const about = require("../routes/about");
     const topicRoutes = require("../routes/topics");
     const advertisementRoutes = require("../routes/advertisements");
     const postRoutes = require("../routes/posts");
@@ -18,7 +17,6 @@ module.exports = {
     app.use(marco);
     app.use(staticRoutes);
     app.use(userRoutes);
-    app.use(about);
     app.use(topicRoutes);
     app.use(advertisementRoutes);
     app.use(postRoutes);
--- src/routes/about.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/about", (req, res, next) => {
-  res.send("About Us");
-});
-
-module.exports = router;
